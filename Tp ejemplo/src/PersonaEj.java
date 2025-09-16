9a08bc1ac16637e462159138003a547d8c8ab40e
QuimeyDAgos <164278589+quimey2312@users.noreply.github.com>
2025-09-16 14:34:48 -0300

    Clase Persona

---
+++ Tp ejemplo/src/PersonaEj.java
@@ -0,0 +1,15 @@
+public class PersonaEj {
+    private String nombre;
+
+    public PersonaEj(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
